4d93e18fd47c56cca442c0e147ccf8e27512c98b
DueForExtinction <99090101+DueForExtinction@users.noreply.github.com>
2023-02-23 20:12:30 -0600

    Added ability to add Addresses
    Addresses now get added correctly.

---
+++ src/main/java/controller/AddAddressServlet.java
@@ -0,0 +1,45 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Address;
+
+/**
+ * Servlet implementation class AddAddressServlet
+ */
+@WebServlet("/addAddressServlet")
+public class AddAddressServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddAddressServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String street = request.getParameter("street");
+		String city = request.getParameter("city");
+		String state = request.getParameter("state");
+		String zip = request.getParameter("zip");
+		
+		Address ads = new Address(street, city, state, zip);
+		AddressItemHelper AddressHelper = new AddressItemHelper();
+		
+		AddressHelper.insertAddress(ads);
+		
+		getServletContext().getRequestDispatcher("/index.html").forward(request, response);
+	}
+
+}
