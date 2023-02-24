c96664811395c4c7d0bd28c58323113f7eb8a0f0
DueForExtinction <99090101+DueForExtinction@users.noreply.github.com>
2023-02-23 18:56:17 -0600

    Create Address.java

---
+++ src/main/java/model/Address.java
@@ -0,0 +1,108 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Brandon Thompson - Brandon Thompson
+ *CIS175 - Spring 2023
+ * Feb 23, 2023
+ */
+public class Address {
+	private int id;
+	private String street;
+	private String city;
+	private String state;
+	private String zip;
+	
+	public Address() {
+		
+	}
+
+	public Address(int id, String street, String city, String state, String zip) {
+		super();
+		this.id = id;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the street
+	 */
+	public String getStreet() {
+		return street;
+	}
+
+	/**
+	 * @param street the street to set
+	 */
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	/**
+	 * @return the zip
+	 */
+	public String getZip() {
+		return zip;
+	}
+
+	/**
+	 * @param zip the zip to set
+	 */
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", street=" + street + ", city=" + city + ", state=" + state + ", zip=" + zip
+				+ "]";
+	}
+	
+	
+}
